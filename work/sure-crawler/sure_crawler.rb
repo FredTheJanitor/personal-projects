8ba860e3a1de075b9ed1aee22e15c37e218c7bd2
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2026-03-25 08:46:29 -0400

    chore: find robot.txt file and look for sitemap

--- work/sure-crawler/sure_crawler.rb
+++ work/sure-crawler/sure_crawler.rb
@@ -1,15 +1,33 @@
 require "open-uri"
+require "zlib"
 
 headers = {
-  "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120.0.0.0",
+  "User-Agent" => "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36",
   "Referer" => "https://www.google.com/search?q=surecritic",
+  "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
   "Accept-Language" => "en-US,en;q=0.9"
 }
 
-remote_base_url = "https://www.surecritic.com/reviews/"
+base_domain = "https://www.surecritic.com"
 
 # I should automate the look up and download of the sitemap
 # so that way this program doesnt require a manual download
 # of the file to run, and so it can run smaller subsequent
 # scans after the first major one.
+# 
+# Define a variable for the sitmap URL
+sitemap_url = "https://www.surecritic.com/sitemaps/sitemap.xml.gz"
+sitemap_filename = "sitemap.xml"
+puts "Downloading sitemap..."
+begin
+  URI.open(sitemap_url, headers) do |compressed_file|
+    gz = Zlib::GzipReader.new(compressed_file)
+    File.open(sitemap_filename, "w") { |f| f.write(gz.read) }
+    gz.close
+  end
+  puts "\tSuccessfully downloaded and decompressed to #{sitemap_filename}"
+  rescue => e
+    puts "\tError: #{e.message}"
+end
+# Navigate to the sitemap with the url
 
